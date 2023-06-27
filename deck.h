62ada1af0216185b54158299c0a34f9fb097d9d2
MesfinGithinji <meshgithinji@gmail.com>
2023-06-27 14:29:02 +0300

    A function that sorts a deck of cards.

--- deck.h
+++ deck.h
@@ -1,13 +1,8 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include <stdio.h>
 #include <stdlib.h>
-
-int _strcmp(const char *s1, const char *s2);
-char get_value(deck_node_t *card);
-void insertion_sort_deck_kind(deck_node_t **deck);
-void insertion_sort_deck_value(deck_node_t **deck);
-void sort_deck(deck_node_t **deck);
 
 /**
  * enum kind_e - Enumeration of card suits.
